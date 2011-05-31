d4aca69e504a24151ff04bc750fdb62a0c0b7383
Jesper Kristensen <cableman@linuxdev.dk>
2011-05-31 19:02:43 +0200

    Cleaned up the javascript code and added functionality to collapse collapsible items

--- script/setup.js
+++ script/setup.js
@@ -1,80 +1,68 @@
-$(function(){
-
-	if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) {
-		$(document).ready(function () {
-			$('label[for]').click(function () {
-				var el = $(this).attr('for');
-				if ($('#' + el + '[type=radio], #' + el + '[type=checkbox]').attr('selected', !$('#' + el).attr('selected'))) {
-					return;
-				} else {
-					$('#' + el)[0].focus();
-				}
-			});
-		});
-	}
-	
-  
-  if(window.orientation != 0){
-	  document.body.className+=' landscape';
+$(document).ready(function(){
+  // If device is Apple change field focus beahavoiur.
+  if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) {
+    $('label[for]').click(function () {
+      var el = $(this).attr('for');
+      if ($('#' + el + '[type=radio], #' + el + '[type=checkbox]').attr('selected', !$('#' + el).attr('selected'))) {
+        return;
+      } else {
+        $('#' + el)[0].focus();
+      }
+    });
   }
-  
-    
+
+  // Add body class if orientation is landscape.
+  if(window.orientation != 0){
+    document.body.className+=' landscape';
+  }
+
+  // Unbind events on reserve-now ?????
   $('.reserve-now').unbind();
 
+  // Disable autocompletion on ting search field.
   $('input.ting-autocomplete').each(function () {
-		$(this).unbind();		
-		
-		$("#search label").inFieldLabels({
-		    fadeOpacity:"0.2",
-		    fadeDuration:"100"			
-		  });
-		
-	});
-  
+    $(this).unbind();
+
+    // Place in-field label.
+    $("#search label").inFieldLabels({
+      fadeOpacity:"0.2",
+      fadeDuration:"100"
+    });		
+  });
+
+  // Enable sticky behaviour to elements (user/status page buttons mostly).
+  $(".sticky-element").sticky({
+    topSpacing:0,
+    className:'sticky'
+  });
+
+  // Detecte collapsible items
+  $('.title-collapsible').toggle(
+    function() {
+      // Display item and remove class.
+      $('.collapsible-info', $(this).parent()).removeClass('collapsed').slideDown(200);
+    },
+    function() {
+      // Hide item and add class.
+      $('.collapsible-info', $(this).parent()).slideUp(200).addClass('collapsed');
+   });
+   // Collapse add collapsible items and add class.
+   $('.collapsible-info').hide().addClass('collapsed');
 });
 
 
-
-/* disable modal login */
+// Disable modal login box.
 Drupal.behaviors.dingLibraryUserLoginDialog = function () {
-	var loginPath = Drupal.settings.basePath + 'user/login';
-	$("a[href^='" + loginPath + "']").unbind('click');
+  var loginPath = Drupal.settings.basePath + 'user/login';
+  $("a[href^='" + loginPath + "']").unbind('click');
 }; 
 
-Drupal.behaviors.addTingAutocomplete = function (context) {
-	
-	
-		
-/*		$(this)
-	.autocomplete(Drupal.settings.tingSearchAutocomplete.path, {
-	// TODO: Consider moving dimensions to CSS for easier customization
-	scrollHeight: 200,
-	 width: 493,
-	 delay: 200,
-	 selectFirst: false,
-	 matchCase: true,
-	 formatResult: function (data) { return data[1]; }
-	 })
-	 .result(function (event) {
-	$(event.target)
-	 .addClass('ac_loading')
-	 .parents('form:first')
-	 .submit();
-	 });
-	 });*/
-};
-
-
+// Add window orientation detection.
 window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
-	if(window.orientation != 0){
-		document.body.className+=' landscape';
-	}
-	else{
-		document.body.className = document.body.className.replace(' landscape','');
-	}
+  if(window.orientation != 0){
+    document.body.className+=' landscape';
+  }
+  else{
+    document.body.className = document.body.className.replace(' landscape','');
+  }
 }, false);
-
-$(document).ready(function(){
-  $(".sticky-element").sticky({topSpacing:0,className:'sticky'});
-  
-});
