2687d9f9a60aeb6cc2a388475eba7e3a6d688de0
Mikkel Hoegh <mikkel@hoegh.org>
2012-03-02 06:33:50 +0100

    Stripped CVS keywords.

--- templates/ting/ting-object.tpl.php
+++ templates/ting/ting-object.tpl.php
@@ -1,5 +1,4 @@
 <?php
-// $Id$
 /**
  * @file ting_object.tpl.php
  *
@@ -34,22 +33,22 @@
     <?php } ?>
   <?php } ?>
   </h1>
-  
+
   <!-- AVAILABILITY -->
 
   <?php // TODO: This should be refactored into the availability module.
     if (ting_object_is($object, 'limited_availability')) { ?>
     <div class="ting-status waiting"><?php print t('waiting for data'); ?></div>
-  <?php } ?>    
-  
+  <?php } ?>
+
   <?php if ($image) { ?>
   <div class="picture">
     <?php print $image; ?>
   </div>
   <?php } ?>
-  
+
   <div class="right-of-pic clear-block">
-    
+
     <div class='creator'>
       <span class='byline'><?php echo ucfirst(t('by')); ?></span>
       <?php print $creators; ?>
@@ -58,22 +57,22 @@
       <?php } ?>
     </div>
     <div class="abstract"><?php print implode(' ; ', format_danmarc2((array)$object->record['dcterms:abstract'][''])) ?></div>
-    
+
     <!-- RESERVE BUTTON -->
-    
+
     <?php if ($buttons) :?>
       <div class="ting-object-buttons">
       <?php print theme('item_list', $buttons, NULL, 'ul', array('class' => 'buttons')) ?>
       </div>
-    <?php endif; ?> 
-    
+    <?php endif; ?>
+
   </div><!-- /.right-of-pic -->
-  
+
   <!-- FLERE INFORMATION -->
   <h2 class="title-collapsible">
     <?php print t('More information'); ?>
   </h2>
-  <div class="additional-info collapsible-info">         
+  <div class="additional-info collapsible-info">
     <?php print theme('item_list', array($object->type), t('Type'), 'ul', array('class' => 'type clear-block')); ?>
     <?php if (!empty($object->record['dc:format'][''])) { ?>
       <?php print theme('item_list', $object->record['dc:format'][''], t('Format'), 'ul', array('class' => 'format clear-block'));?>
@@ -81,8 +80,8 @@
     <?php if (!empty($object->record['dcterms:isPartOf'][''])) { ?>
       <?php print theme('item_list', $object->record['dcterms:isPartOf'][''], t('Available in'), 'ul', array('class' => 'is-part-of clear-block'));?>
     <?php } ?>
-  
-  
+
+
     <?php if (!empty($object->language)) { ?>
       <?php print theme('item_list', array($object->language), t('Language'), 'ul', array('class' => 'language'));?>
     <?php } ?>
@@ -92,7 +91,7 @@
     <?php if (!empty($object->record['dc:language']['oss:subtitles'])) { ?>
       <?php print theme('item_list', $object->record['dc:language']['oss:subtitles'], t('Subtitles'), 'ul', array('class' => 'language'));?>
     <?php } ?>
-  
+
     <?php if (!empty($object->record['dc:subject']['oss:genre'])) { ?>
       <?php print theme('item_list', $object->record['dc:subject']['oss:genre'], t('Genre'), 'ul', array('class' => 'subject'));?>
     <?php } ?>
@@ -105,7 +104,7 @@
     <?php if (!empty($object->record['dcterms:spatial'][''])) { ?>
       <?php print theme('item_list', $object->record['dcterms:spatial'][''], NULL, 'ul', array('class' => 'spatial')); ?>
     <?php } ?>
-  
+
     <?php if (!empty($object->record['dc:contributor']['oss:dkind'])) { ?>
       <?php print theme('item_list', $object->record['dc:contributor']['oss:dkind'], t('Reader'), 'ul', array('class' => 'contributor'));?>
     <?php } ?>
@@ -115,22 +114,22 @@
     <?php if (!empty($object->record['dc:contributor']['oss:mus'])) { ?>
       <?php print theme('item_list', $object->record['dc:contributor']['oss:mus'], t('Musician'), 'ul', array('class' => 'contributor'));?>
     <?php } ?>
-  
+
     <?php if (!empty($object->record['dcterms:hasPart']['oss:track'])) { ?>
       <?php print theme('item_list', $object->record['dcterms:hasPart']['oss:track'], t('Contains'), 'ul', array('class' => 'contains'));?>
     <?php } ?>
-  
+
     <?php if (!empty($object->record['dcterms:isReferencedBy'][''])) { ?>
       <?php print theme('item_list', $object->record['dcterms:isReferencedBy'][''], t('Referenced by'), 'ul', array('class' => 'referenced-by'));?>
     <?php } ?>
-  
-  
+
+
     <?php if (!empty($object->record['dc:description'])) { ?>
       <?php foreach ($object->record['dc:description'] as $type => $dc_description) { ?>
         <?php print theme('item_list', $dc_description, t('Description'), 'ul', array('class' => 'description'));?>
       <?php } ?>
     <?php } ?>
-  
+
     <?php if (!empty($object->record['dc:source'][''])) { ?>
       <?php print theme('item_list', $object->record['dc:source'][''], t('Original title'), 'ul', array('class' => 'titles'));?>
     <?php } ?>
@@ -140,11 +139,11 @@
     <?php if (!empty($object->record['dcterms:isReplacedBy'][''])) { ?>
       <?php print theme('item_list', $object->record['dcterms:isReplacedBy'][''], t('Later title'), 'ul', array('class' => 'titles'));?>
     <?php } ?>
-  
+
     <?php if (!empty($object->record['dc:identifier']['dkdcplus:ISBN'])) { ?>
       <?php print theme('item_list', $object->record['dc:identifier']['dkdcplus:ISBN'], t('ISBN no.'), 'ul', array('class' => 'identifier'));?>
     <?php } ?>
-  
+
     <?php
     if (!empty($object->record['dc:identifier']['dcterms:URI'])) {
       $uris = array();
@@ -154,11 +153,11 @@
       print theme('item_list', $uris, t('Host publication'), 'ul', array('class' => 'identifier'));
     }
     ?>
-  
+
     <?php if (!empty($object->record['dkdcplus:version'][''])) { ?>
       <?php print theme('item_list', $object->record['dkdcplus:version'][''], t('Version'), 'ul', array('class' => 'version'));?>
     <?php } ?>
-  
+
     <?php if (!empty($object->record['dcterms:extent'][''])) { ?>
       <?php print theme('item_list', $object->record['dcterms:extent'][''], t('Extent'), 'ul', array('class' => 'version'));?>
     <?php } ?>
@@ -168,7 +167,7 @@
     <?php if (!empty($object->record['dc:rights'][''])) { ?>
       <?php print theme('item_list', $object->record['dc:rights'][''], t('Rights'), 'ul', array('class' => 'rights'));?>
     <?php } ?>
-  
+
   </div><!-- /additional-info -->
-  
+
 </div><!-- /#ting-item -->
